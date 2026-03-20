529ba540f194b223a25c38cb4c9174b337a519b4
Matteo Luigi Tommasi <matteoluigitommasi@gmail.com>
2026-03-20 15:11:15 +0100

    created the tools to comunicate with backendw

--- backend/src/lib/swagger.ts
+++ backend/src/lib/swagger.ts
@@ -9,9 +9,15 @@ export  const initSwagger = (app: ReturnType<typeof express>) => {
         title: "API Documentation",
         version: "1.0.0",
       },
+      basePath: "/",
+
     },
     apis: ["./src/**/*.ts"], // Adjust the path to your route files
   });
 
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/swagger.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
 };
